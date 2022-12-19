dc5245a493e5c96a6bd1a70a9f6b5cf24bb7f278
ChellZhang <chell.czhang@gmail.com>
2022-12-19 14:01:00 -0800

    removed the CORS from Yelp component

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -2,7 +2,7 @@ const apiKey = '1kNnlISbb0j84pa_cPdiSv_jl6Zj_ChS8Sxf3qXRx7GA8KAkpCp_4d3qqCVWfgze
 
 const Yelp = {
     search(term, location, sortBy){
-        return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
+        return fetch(`https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
         {
             headers:{
                 Authorization: `Bearer ${apiKey}`
